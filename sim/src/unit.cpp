fe6772104a3269e5e2834f64b5c728c2e0239675
ilevy <itayill01@gmail.com>
2022-05-21 00:45:37 +0300

    Use ammo macro

--- sim/src/unit.cpp
+++ sim/src/unit.cpp
@@ -30,21 +30,21 @@ const std::map<std::string, Unit> UNIT_TYPES = {
 };
 
 const UnitMeta UNITS_META[] = {
-    {Unit::hoplite, 56, 18, 0, -1, 1, 1, true},
-    {Unit::steam_giant, 184, 42, 0, -1, 3, 3, false},
-    {Unit::spearman, 13, 4, 0, -1, 0, 1, true},
-    {Unit::swordsman, 18, 10, 0, -1, 0, 1, true},
+    {Unit::hoplite, 56, 18, 0, NO_AMMO, 1, 1, true},
+    {Unit::steam_giant, 184, 42, 0, NO_AMMO, 3, 3, false},
+    {Unit::spearman, 13, 4, 0, NO_AMMO, 0, 1, true},
+    {Unit::swordsman, 18, 10, 0, NO_AMMO, 0, 1, true},
     {Unit::slinger, 8, 2, 3, 5, 0, 1, true},
     {Unit::archer, 16, 5, 5, 3, 0, 1, true},
     {Unit::carbineer, 12, 3, 29, 3, 0, 4, true},
-    {Unit::ram, 88, 12, 80, -1, 0, 5, false},
+    {Unit::ram, 88, 12, 80, 0, NO_AMMO, 5, false},
     {Unit::catapult, 54, 4, 133, 5, 0, 5, false},
     {Unit::mortar, 32, 10, 270, 3, 0, 5, false},
     {Unit::gyrocopter, 29, 0, 17, 4, 0, 1, false},
     {Unit::bombardier, 40, 0, 48, 2, 0, 2, false},
     {Unit::cook, 0, 0, 0, 0, 0, 0, true},
     {Unit::doctor, 0, 0, 0, 0, 0, 0, true},
-    {Unit::barbarian, 12, 7, 0, -1, 1, 1, true},
+    {Unit::barbarian, 12, 7, 0, NO_AMMO, 1, 1, true},
 };
 
 bool is_ranged(Unit type) {

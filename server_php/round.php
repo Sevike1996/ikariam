f3beb1fe45b2c50cfa4d674f7aa6f4cb850fdc2a
ilevy <itayill01@gmail.com>
2023-05-26 00:30:27 +0300

    Remove php server

--- server_php/round.php
+++
@@ -1,9 +0,0 @@
-<?php
-$round_id = $_GET["id"];
-$dir = substr($round_id, 0, 2);
-$path = substr($round_id, 2);
-
-$full_path = "/tmp/ikariam/rounds/".$dir."/".$path;
-$content = file_get_contents($full_path, "r");
-echo $content;
-?>

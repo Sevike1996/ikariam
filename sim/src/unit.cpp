87ee5898003cbb3023f8a4bf24f14f4bb663cc34
ilevi <itayill01@gmail.com>
2023-11-17 02:39:19 +0200

    Fix ram armor/ammo mismatch

--- sim/src/unit.cpp
+++ sim/src/unit.cpp
@@ -38,13 +38,13 @@ const UnitMeta UNITS_META[] = {
     {Unit::slinger, 8, 2, 3, 5, 0, 1, true},
     {Unit::archer, 16, 5, 5, 3, 0, 1, true},
     {Unit::carbineer, 12, 3, 29, 3, 0, 4, true},
-    {Unit::ram, 88, 12, 80, 0, NO_AMMO, 5, false},
+    {Unit::ram, 88, 12, 80, NO_AMMO, 0, 5, false},
     {Unit::catapult, 54, 4, 133, 5, 0, 5, false},
     {Unit::mortar, 32, 10, 270, 3, 0, 5, false},
     {Unit::gyrocopter, 29, 0, 17, 4, 0, 1, false},
     {Unit::bombardier, 40, 0, 48, 2, 0, 2, false},
-    {Unit::cook, 0, 0, 0, 0, 0, 0, true},
-    {Unit::doctor, 0, 0, 0, 0, 0, 0, true},
+    {Unit::cook, 0, 0, 0, 0, 0, 0, true}, // TODO support healing
+    {Unit::doctor, 0, 0, 0, 0, 0, 0, true}, // TODO suppport morale
     {Unit::barbarian, 12, 7, 0, NO_AMMO, 1, 1, true},
 };
 
